0449b3f84235e116d14b2d00ea55bc0b57294a2e
dnxtpro <161753397+dnxtpro@users.noreply.github.com>
2024-11-24 22:44:09 +0100

    checking the members

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ import { useInView } from "react-intersection-observer";
 import { Mail, Building } from "lucide-react";
 import Loading from "./Loading";
 import { useEffect } from "react";
-
-
+import { AnimatePresence } from "framer-motion";
 import ParticlesComponent from "./Background";
+import { X } from "lucide-react";
 
 const teamMembers = [
   {
@@ -39,7 +39,8 @@ const teamMembers = [
     image: "chemcham.jpg",
     gridCol: "",
     born: "Née à Tétouan",
-    details: "A obtenu son diplôme de gestion des entreprises en 2005",
+    details:
+      "A obtenu son diplôme de gestion des entreprises en 2005. Avant de devenir femme d’affaires et de bâtir sa société, elle a travaillé dans plusieurs grandes entreprises en tant responsable commercial, ce qui lui permis d’accumuler une expérience riche et importantedans le commerce, ainsi qu’un savoir-faire dans le domainedes affaires, et enfin de décider ouvrir son propre projet,fabrication et vente de chocolat de haute qualité.",
   },
   {
     role1: "Vice Trésoriére",
@@ -67,7 +68,8 @@ const teamMembers = [
     image: "mariam.jpg",
     gridCol: "col-start-3",
     born: "Née à Tétouan",
-    details: "Ingénieure en informatique,diplômé de l'ENSI de Caen en 1989",
+    details:
+      "Ingénieure en informatique, elle a obtenu son diplôme de l'ENSI de Caen en 1989. Elle a commencé sa carrière en tant qu'ingénieure de développement informatique dans une société à Toulouse, où elle a travaillé de 1990 à 1999. Par la suite, elle a intégré la fonction publique marocaine dans l'enseignement supérieur. En 2009, elle a soutenu son doctorat en informatique à l'INSA de Rouen. Actuellement, elle est professeure de l'enseignement supérieur à l'ENSA de Tanger, où elle forme les futures générations d'ingénieurs dans le domaine des réseaux informatiques.",
   },
 ];
 
@@ -86,6 +88,11 @@ function App() {
   const opa = useTransform(scrollY, [1700, 1800], [0, 1]);
 
   const [isLoading, setIsLoading] = useState(true);
+  const [expandedMember, setExpandedMember] = useState(null);
+
+  const handleMemberClick = (index) => {
+    setExpandedMember(expandedMember === index ? null : index);
+  };
 
   useEffect(() => {
     const handleResize = () => setIsMobile(window.innerWidth < 768);
@@ -106,37 +113,34 @@ function App() {
 
   return (
     <div className="min-h-screen bg-gradient-to-b from-amber-300 via-amber-300/50 to-black relative">
-      <ParticlesComponent id="particles"/>
-
+      <ParticlesComponent id="particles" />
 
       {isLoading ? (
         <Loading />
       ) : (
-        
         <div className="content bg-black/90">
-          
-          
           <motion.div
-          initial={{opacity:0}}
-          animate={{opacity:1}}
-          transition={{duration:1}}
-          className="w-full min-h-screen ">
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+            className="w-full min-h-screen "
+          >
             <motion.header
               className="relative overflow-hidden py-24 md:py-32 z-30"
               style={{ y: y1 }}
             >
               <motion.div
-                initial={{ opacity: 0.5, y:  0}}
+                initial={{ opacity: 0.5, y: 0 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.8 }}
                 className="relative  container mx-auto px-4"
               >
                 <div className="text-center text-white  bg-[#191508]  ">
                   <h1 className="mb-2 text-4xl font-bold tracking-tight text-gray-100 md:text-5xl lg:text-6xl">
-                     Khadija Tnana
+                    Khadija Tnana
                   </h1>
                   <h2 className="text-2xl font-medium text-dorado md:text-3xl lg:text-4xl">
-                  FONDATION 
+                    FONDATION
                   </h2>
                   <div className="mt-8 flex justify-center">
                     <div className="h-1 w-16 bg-dorado"></div>
@@ -154,11 +158,11 @@ function App() {
                 style={{ y: y2, opacity: opas }}
               >
                 <h2 className="text-3xl  font-light leading-tight text-white md:text-4xl lg:text-5xl">
-                  UN ESPACE DÉDIÉ  À {" "}
+                  UN ESPACE DÉDIÉ À{" "}
                   <span className="mt-2 block font-bold text-dorado md:text-5xl lg:text-6xl">
-                    l&apos;ÉCHANGE 
+                    l&apos;ÉCHANGE
                   </span>{" "}
-                  ET À  LA PROMOTION DE LA CRÉATIVITÉ AU MAROC
+                  ET À LA PROMOTION DE LA CRÉATIVITÉ AU MAROC
                 </h2>
               </motion.div>
 
@@ -204,7 +208,7 @@ function App() {
               className="flex md:flex-row flex-col overflow-x-hidden items-center mx-8 px-4 gap-12 "
             >
               <motion.img
-                style={{ x: isMobile ? 0 : x1  }}
+                style={{ x: isMobile ? 0 : x1 }}
                 whileHover={{ scale: 1.05 }}
                 transition={{ duration: 0.3 }}
                 src="mujer.jpg"
@@ -213,16 +217,14 @@ function App() {
               />
 
               <motion.h2
-              style={{
-              x: isMobile ? 0 : x2, // En móviles, x es fijo (0); en pantallas grandes, x2 es dinámico
-               }}
+                style={{
+                  x: isMobile ? 0 : x2, // En móviles, x es fijo (0); en pantallas grandes, x2 es dinámico
+                }}
                 className="text-3xl z-30 order-1 font-light leading-tight text-white md:text-4xl lg:text-5xl"
               >
                 SOUTENIR LES TALENTS CRÉATIFS, PROMOUVOIR LA CULTURE MAROCAINE,
                 ET CÉLÉBRER LA{" "}
-                <span className="text-dorado">
-                DIVERSITÉ ARTISTIQUE
-                </span>
+                <span className="text-dorado">DIVERSITÉ ARTISTIQUE</span>
               </motion.h2>
             </div>
 
@@ -235,17 +237,11 @@ function App() {
                 <h2 className="text-3xl font-light leading-tight text-white md:text-4xl lg:text-5xl">
                   ENCOURAGER LA
                   <span className="mt-2 block font-bold text-dorado md:text-5xl lg:text-6xl">
-                  CRÉATIVITÉ{" "}
-                   
-                      FEMININE
-                    
+                    CRÉATIVITÉ FEMININE
                   </span>{" "}
                   ET SOUTENIR LA
                   <span className="mt-2 block font-bold text-dorado md:text-5xl lg:text-6xl">
-                    
-                      JEUNESSE
-                    
-                      ARTISTIQUE
+                    JEUNESSE ARTISTIQUE
                   </span>
                 </h2>
               </div>
@@ -261,48 +257,103 @@ function App() {
 
             <ObjectiveSection />
 
-            <section className="px-4 py-16">
-              <motion.h2
-                initial={{ opacity: 0, y: 20 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                className="text-4xl font-playfair text-white text-center mb-16"
-              >
+            <section className="px-4 py-16 relative bg-black min-h-screen">
+              <h2 className="text-4xl font-playfair text-white text-center mb-16">
                 Notre Équipe
-              </motion.h2>
-              <div className="container mx-auto md:grid flex flex-col md:grid-cols-3 space-y-8 gap-8 mon">
+              </h2>
+              <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 relative">
                 {teamMembers.map((member, index) => (
                   <motion.div
                     key={member.name}
-                    initial={{ opacity: 0, y: 20 }}
-                    whileInView={{ opacity: 1, y: 0 }}
-                    transition={{ delay: index * 0.1, duration: 0.5 }}
-                    className={`team-member ${member.gridCol}`}
+                    layout
+                    initial={{ opacity: 0, scale: 0.8 }}
+                    animate={{
+                      opacity: 1,
+                      scale: 1,
+                      transition: { duration: 0.5 },
+                    }}
+                    className="team-member cursor-pointer"
+                    onClick={() => handleMemberClick(index)}
                   >
-                    <img
-                      src={member.image}
-                      alt={member.name}
-                      className="grayscale mon"
-                    />
-                    <div className="team-member-content">
-                      <div className="bg-gradient-to-t from-black/90 to-transparent absolute inset-0" />
-                      <div className="relative">
-                        <h2>
-                          {member.name}
-                          <br />
-                          <span className="text-dorado">{member.role1}</span>
-                        </h2>
-                        <p>
-                          {member.role}
-                          <br />
-                          {member.born}
-                          <br />
-                          {member.details}
-                        </p>
+                    <div className="relative overflow-hidden rounded-2xl">
+                      <img
+                        src={member.image}
+                        alt={member.name}
+                        className="w-full h-[400px] grayscale mon object-cover transition-all duration-500"
+                      />
+                      <div className="team-member-content">
+                        <div className="bg-gradient-to-t from-black/90 to-transparent absolute inset-0" />
+                        <div className="relative">
+                          <h2>
+                            {member.name}
+                            <br />
+                            <span className="text-dorado">{member.role1}</span>
+                          </h2>
+                          <p>
+                            {member.role}
+                            <br />
+                            {member.born}
+                            <br />
+                          </p>
+                        </div>
                       </div>
                     </div>
                   </motion.div>
                 ))}
               </div>
+              <AnimatePresence>
+                {expandedMember !== null && (
+                  <>
+                    <motion.div
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      exit={{ opacity: 0 }}
+                      transition={{ duration: 0.3 }}
+                      className="fixed inset-0 bg-gradient-to-t from-black/90 to-transparent z-40"
+                      onClick={() => setExpandedMember(null)}
+                    />
+                    <motion.div
+                      layoutId={`member-${expandedMember}`}
+                      initial={{ opacity: 0, scale: 0.8 }}
+                      animate={{ opacity: 1, scale: 1 }}
+                      exit={{ opacity: 0, scale: 0.8 }}
+                      transition={{ duration: 0.3 }}
+                      className="fixed inset-0 z-50 flex items-center  justify-center  bg-gradient-to-t from-black/90 to-transparent p-4"
+                    >
+                      <div className="bg-black rounded-2xl overflow-hidden max-w-2xl w-full shadow-2xl modal-content">
+                        <div className="relative h-[60vh]">
+                          <img
+                            src={teamMembers[expandedMember].image}
+                            alt={teamMembers[expandedMember].name}
+                            className="w-full h-[40vh] object-cover"
+                          />
+                          <div className="absolute inset-0 bg-gradient-to-t from-black via-black/90 to-transparent" />
+                          <div className="absolute inset-0  p-8 flex flex-col justify-end text-dorado">
+                            <h3 className="text-3xl font-medium text-white mb-2">
+                              {teamMembers[expandedMember].name}
+                            </h3>
+                            <p className="text-3xl font-bold xl text-dorado mb-4">
+                              {teamMembers[expandedMember].role1}
+                            </p>
+                            <p className="text-lg text-white mb-2">
+                              {teamMembers[expandedMember].role}
+                            </p>
+                            <p className="text-base text-dorado">
+                              {teamMembers[expandedMember].details}
+                            </p>
+                          </div>
+                        </div>
+                        <button
+                          className="absolute top-4 right-4 bg-white text-black p-2 rounded-full shadow-lg"
+                          onClick={() => setExpandedMember(null)}
+                        >
+                          <X size={24} />
+                        </button>
+                      </div>
+                    </motion.div>
+                  </>
+                )}
+              </AnimatePresence>
             </section>
             <footer className="bg-black py-8">
               <div className="container mx-auto px-4 text-center">
